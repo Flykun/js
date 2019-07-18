30a47254d555cc863198089e1660cc27f26309f4
Flukun <1689890728@qq.com>
2019-07-18 12:02:23 +0800

    updata

--- bubble/bubble.js
+++ bubble/bubble.js
@@ -4,8 +4,21 @@ var scores = [60, 50, 60, 58, 54, 54,
     69, 64, 66, 55, 52, 61,
     46, 31, 57, 52, 44, 18,
     41, 53, 55, 61, 51, 44];
+var highScore = 0;
 var output;
 for (var i = 0; i < scores.length; i++) {
     output = "Bubble solution #" + i + " score: " + scores[i];
     console.log(output)
+    if (scores[i] > highScore) {
+        highScore = scores[i];
+    }
 }
+console.log("Bubbles tests: " + scores.length);
+console.log("Highest bubbles score: " + highScore);
+var bestSolutions = [];
+for (var i=0; i < scores.length; i++) {
+    if (scores[i] == highScore) {
+        bestSolutions.push(i);
+    }
+}
+console.log("Solutions with the highest score: " + bestSolutions);
